cc1f4f3d11bc37e1195b245ac2bd6f7e95d97917
William Keller <william@arda.net>
2021-08-31 14:09:39 -0500

    Cleared out main actor sheet
    - Everything is still present, just commented out
    - Renamed the tabs

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const WKRM = {};
+
+// Define constants here, such as:
+WKRM.foobar = {
+  'bas': 'WKRM.bas',
+  'bar': 'WKRM.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ WKRM.abilities = {
+  "str": "WKRM.AbilityStr",
+  "dex": "WKRM.AbilityDex",
+  "con": "WKRM.AbilityCon",
+  "int": "WKRM.AbilityInt",
+  "wis": "WKRM.AbilityWis",
+  "cha": "WKRM.AbilityCha"
+};
+
+WKRM.abilityAbbreviations = {
+  "str": "WKRM.AbilityStrAbbr",
+  "dex": "WKRM.AbilityDexAbbr",
+  "con": "WKRM.AbilityConAbbr",
+  "int": "WKRM.AbilityIntAbbr",
+  "wis": "WKRM.AbilityWisAbbr",
+  "cha": "WKRM.AbilityChaAbbr"
+};
